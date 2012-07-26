bffcb17d12e911df6119c8cc765701bd80e7528e
Andy Chu <andy@chubot.org>
2012-07-25 22:30:02 -0700

    BUILD.sh can make a release.
    In other repos, it would get dependencies, but tin has no deps now.  It may have
    coopt as a dependency later.

---
+++ BUILD.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# do.sh
+#
+# Usage:
+#   ./do.sh <action>
+
+readonly CURRENT_VERSION=0.1
+
+release() {
+  local out=tin-$CURRENT_VERSION.zip
+  rm $out
+  # Don't include this do.sh script!
+  zip $out \
+    bin/*.py bin/*.sh BUILD.sh PROVIDES.sh
+  unzip -l $out
+}
+
+get-uploader() {
+  mkdir _tmp
+  wget -O _tmp/googlecode_upload.py \
+    http://support.googlecode.com/svn/trunk/scripts/googlecode_upload.py
+  chmod +x _tmp/googlecode_upload.py
+}
+
+upload() {
+  _tmp/googlecode_upload.py \
+      -s "Tin $CURRENT_VERSION" \
+      -p tintool \
+      tin-$CURRENT_VERSION.zip
+}
+
+"$@"
