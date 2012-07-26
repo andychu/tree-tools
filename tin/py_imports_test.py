67e844257e960cbad0e6129478a73dabadb4ea62
Andy Chu <andy@chubot.org>
2012-07-26 01:29:14 -0700

    Copied unit tests from Poly repo

---
+++ tin/py_imports_test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python -S
+"""
+py_imports_test.py: Tests for py_imports.py
+"""
+
+__author__ = 'Andy Chu'
+
+
+import sys
+import unittest
+
+import py_imports  # module under test
+
+
+class PyImportsTest(unittest.TestCase):
+
+  def setUp(self):
+    pass
+
+  def tearDown(self):
+    pass
+
+  def testModules(self):
+    pairs = [ ( 'poly.util',
+               'poly/util.py'),
+              ( 'simplejson', 
+                '/home/andy/dev/simplejson-2.1.5/simplejson/__init__.py')
+              ]
+    print list(py_imports.ModuleToRelativePath(pairs))
+
+
+if __name__ == '__main__':
+  unittest.main()
