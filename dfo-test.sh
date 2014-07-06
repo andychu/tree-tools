ae89805837a51586a92a2ea15aecceca9753f7f3
Andy Chu <andy@chubot.org>
2014-07-06 01:08:47 -0700

    Start using test framework.
    Move other stuff over to a perftest.

--- dfo-test.sh
+++ dfo-test.sh
@@ -3,7 +3,9 @@
 # Usage:
 #   ./dfo-test.sh <function name>
 
-create() {
+. ~/hg/taste/taste.sh
+
+create-tree() {
   local out=_tmp/pack
   mkdir -p $out
 
@@ -25,15 +27,17 @@ create() {
   bin/ftree $out
 }
 
-pack() {
+test-pack() {
   local out=_tmp/foo.dfo
   bin/dfo pack _tmp/pack >$out
+  expect $? -eq 0
+
   ls -al $out
   cat $out
   echo
 }
 
-unpack() {
+test-unpack() {
   local out=_tmp/unpack
   rm -rf $out
   cat _tmp/foo.dfo | bin/dfo unpack $out
@@ -55,67 +59,4 @@ unpack() {
     bin/dfo unpack $out
 }
 
-# Results
-#
-# Original R .tar.gz    25.4 MB
-# dfo.gz                25.5 MB   4.08 s
-# dfo.bzip2             21.6 MB  17.24 s
-# dfo.xz                18.9 MB  56.99 s  !
-
-readonly R_TAR=~/hg/poly-runtimes/src/R-2.15.3.tar.gz
-
-make-big-dfo() {
-  set -x
-
-  local tar=$R_TAR
-  ls -al $(readlink -f $tar)
-  local dir=_tmp/R
-  if ! test -d $dir; then
-    mkdir $dir
-    tar --extract --checkpoint=1000 --file $tar --directory $dir
-  fi
-
-  du -hs $dir
-
-  local out=_tmp/R.dfo
-  if ! test -f $out; then
-    bin/dfo pack $dir >$out
-  fi
-
-  ls -al $out
-
-  # gzip it
-  if ! test -f $out.gz; then
-    time gzip <$out >$out.gz
-  fi
-  ls -al $out.gz
-
-  # bzip2 it
-  if ! test -f $out.bz2; then
-    time bzip2 <$out >$out.bz2
-  fi
-  ls -al $out.bz2
-
-  # xz it
-  if ! test -f $out.xz; then
-    time xz <$out >$out.xz
-  fi
-  ls -al $out.xz
-}
-
-# Results:
-#
-# .tar   72.29 MB
-# .dfo   72.31 MB
-#
-# Very slightly bigger, because of all the 40 byte checksums.  Could probably
-# get it down by using 20 byte checksums.  Hm.
-
-compare-uncompressed() {
-  local out=_tmp/R-2.15.3.tar
-  time gzip -d <$R_TAR >$out
-  ls -al $out
-  ls -al _tmp/R.dfo
-}
-
-"$@"
+taste-main "$@"
